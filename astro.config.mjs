0cb25b12accba1123ac1860e68478ca2edf52469
Krishna Santosh <75202541+krishna-santosh@users.noreply.github.com>
2025-11-13 11:52:20 +0530

    feat: add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,14 @@ import svelte from "@astrojs/svelte";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   // Change this to your deployment url
   site: "https://canvas.krish.fyi",
   prefetch: true,
-  integrations: [icon(), svelte()],
+  integrations: [icon(), svelte(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
